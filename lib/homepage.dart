04e2a4e9ce2abf7f049a262182c6928e79cbe3cb
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2023-06-20 20:37:29 +0600

    Initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_rounded),
+            label: 'Statistics',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wallet_rounded),
+            label: 'Wallet',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.person_rounded),
+          //   label: 'Profile',
+          // ),
+        ],
+      ),
+    );
+  }
+}
