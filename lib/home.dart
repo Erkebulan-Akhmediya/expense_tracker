b967ee13c537cbc3521e8fa0e691daf26c5dba52
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2023-06-21 03:06:00 +0600

    home page #2

--- lib/home.dart
+++ lib/home.dart
@@ -44,6 +44,34 @@ class Home extends StatelessWidget {
               ),
             ),
           ),
+          const Column(
+            children: <Widget>[
+              Transaction(
+                imageUrl: 'https://play-lh.googleusercontent.com/TBRwjS_qfJCSj1m7zZB93FnpJM5fSpMA_wUlFDLxWAb45T9RmwBvQd5cWR5viJJOhkI',
+                name: 'Netflix',
+                date: 'Yesterday',
+                amount: -700,
+              ),
+              Transaction(
+                imageUrl: 'https://play-lh.googleusercontent.com/bDCkDV64ZPT38q44KBEWgicFt2gDHdYPgCHbA3knlieeYpNqbliEqBI90Wr6Tu8YOw',
+                name: 'PayPal',
+                date: '21 Jun, 2023',
+                amount: 1500,
+              ),
+              Transaction(
+                imageUrl: 'https://logosandtypes.com/wp-content/uploads/2020/11/Shopify.png',
+                name: 'Shopify',
+                date: '12 Jun, 2023',
+                amount: 1000,
+              ),
+              Transaction(
+                imageUrl: 'https://play-lh.googleusercontent.com/UrY7BAZ-XfXGpfkeWg0zCCeo-7ras4DCoRalC_WXXWTK9q5b0Iw7B0YQMsVxZaNB7DM',
+                name: 'Shopify',
+                date: '01 Jun, 2023',
+                amount: -1200,
+              ),
+            ],
+          ),
         ],
       ),
     );
@@ -154,6 +182,115 @@ class IncomeExpenses extends StatelessWidget {
             )
           ),
         ],
+      ),
+    );
+  }
+
+}
+
+class Transaction extends StatefulWidget {
+  const Transaction({
+    super.key,
+    required this.imageUrl,
+    required this.name,
+    required this.date,
+    required this.amount,
+  });
+
+  final String imageUrl;
+  final String name;
+  final String date;
+  final int amount;
+
+  @override
+  State<Transaction> createState() => _TransactionState();
+}
+
+class _TransactionState extends State<Transaction> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Image(
+                    width: 60,
+                    height: 60,
+                    image: NetworkImage(widget.imageUrl),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      widget.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(widget.date),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TransactionAmount(
+                  amount: widget.amount,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TransactionAmount extends StatefulWidget {
+  const TransactionAmount({
+    super.key,
+    required this.amount,
+  });
+
+  final int amount;
+
+  @override
+  State<StatefulWidget> createState() => _TransactionAmountState();
+}
+
+class _TransactionAmountState extends State<TransactionAmount> {
+  String sign(int num) {
+    if (num > 0) {
+      return '+';
+    } else {
+      return '';
+    }
+  }
+
+  Color colors(int num) {
+    if (num > 0) {
+      return Colors.green;
+    } else {
+      return Colors.red;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      sign(widget.amount) + widget.amount.toString(),
+      style: TextStyle(
+        color: colors(widget.amount),
+        fontSize: 20,
       ),
     );
   }
